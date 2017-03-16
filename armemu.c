29827d6ba7187fde4414b2e991870189a6afa302
Mathieu Clement <mclement2@dons.usfca.edu>
2017-03-15 22:41:16 -0700

    Implement conditions

--- armemu.c
+++ armemu.c
@@ -419,42 +419,87 @@ void armemu_one_load_store_multiple (struct state* s, struct load_store_multiple
     s->regs[instr->rn] = (unsigned int) addr;
 } // end armemu_one_load_store_multiple
 
+#define COND_EQ 0
+#define COND_NE 1
+#define COND_LS 9
+#define COND_GE 10
+#define COND_LT 11
+#define COND_LE 13
+#define COND_AL 14
+
+bool condition_is_true (struct state* s, unsigned int cond)
+{
+    // bit numbers from https://www.heyrick.co.uk/armwiki/The_Status_register
+    unsigned int cpsr = s->cpsr;
+    unsigned int n = select_bits(cpsr, 31, 31);
+    unsigned int z = select_bits(cpsr, 30, 30);
+    unsigned int c = select_bits(cpsr, 29, 29);
+    unsigned int v = select_bits(cpsr, 28, 28);
+    // unsigned int q = select_bits(cpsr, 27, 27);
+
+    switch (cond) {
+        case COND_EQ:
+            return z == 0;
+        case COND_NE:
+            return z == 1;
+        case COND_LS:
+            return c == 0 || z == 1;
+        case COND_GE:
+            return n == v;
+        case COND_LT:
+            return n != v;
+        case COND_LE:
+            return z == 1 || n != v;
+        case COND_AL:
+            return true;
+        default:
+            fprintf(stderr, "Unknown condition 0x%02x\n", cond);
+            exit(EXIT_FAILURE);
+    }
+}
+
 void armemu_one (struct state* s)
 {
     unsigned int* pc_addr = (unsigned int*) s->regs[PC];
     debug("PC is at address 0x%02x", s->regs[PC]);
     struct dp_instr dp_instr = decode_dp_instr(*pc_addr);
     //print_instr(&dp_instr);
+    
+    if (condition_is_true(s, dp_instr.cond)) {
+        debug("Condition %d is true", dp_instr.cond);
 
-    switch (dp_instr.op) {
-        case 0x00: // Data processing
-            armemu_one_dp(s, &dp_instr);
-            break;
-        case 0x01: // Data transfer (LDR, STR)
-            {
-                struct load_store_instr instr = decode_load_store_instr(*pc_addr);
-                armemu_one_load_store(s, &instr);
+        switch (dp_instr.op) {
+            case 0x00: // Data processing
+                armemu_one_dp(s, &dp_instr);
                 break;
-            }
-        case 0x02: // Branch and link or STM/LDM
-            { 
-                unsigned int code = select_bits(*pc_addr, 27, 25);
-                if (code == 0x5) { // Branch
-                    struct branch_link_instr bl_instr = decode_branch_link_instr(*pc_addr);
-                    armemu_one_branch(s, &bl_instr);
-                } else if (code == 0x4) { // STM/LDM
-                    struct load_store_multiple_instr lsm_instr = decode_load_store_multiple_instr(*pc_addr);
-                    armemu_one_load_store_multiple(s, &lsm_instr);
+            case 0x01: // Data transfer (LDR, STR)
+                {
+                    struct load_store_instr instr = decode_load_store_instr(*pc_addr);
+                    armemu_one_load_store(s, &instr);
                     break;
-                } else {
-                    fprintf(stderr, "Unknown instruction %02x \n", *pc_addr);
-                    exit(EXIT_FAILURE);
                 }
-                break;
-            }
-        default:
-            fprintf(stderr, "Unknown instruction type (op) %02x.\n", dp_instr.op);
-            exit(EXIT_FAILURE);
+            case 0x02: // Branch and link or STM/LDM
+                { 
+                    unsigned int code = select_bits(*pc_addr, 27, 25);
+                    if (code == 0x5) { // Branch
+                        struct branch_link_instr bl_instr = decode_branch_link_instr(*pc_addr);
+                        armemu_one_branch(s, &bl_instr);
+                    } else if (code == 0x4) { // STM/LDM
+                        struct load_store_multiple_instr lsm_instr = decode_load_store_multiple_instr(*pc_addr);
+                        armemu_one_load_store_multiple(s, &lsm_instr);
+                        break;
+                    } else {
+                        fprintf(stderr, "Unknown instruction %02x \n", *pc_addr);
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                }
+            default:
+                fprintf(stderr, "Unknown instruction type (op) %02x.\n", dp_instr.op);
+                exit(EXIT_FAILURE);
+        } // end switch
+    } else {
+        debug("Condition %d is false", dp_instr.cond);
     }
 
     // Update PC
